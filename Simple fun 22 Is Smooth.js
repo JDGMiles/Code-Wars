b6753c637655689781e44fe075a200b6a5a6eb0e
James Miles <jimdgmiles@gmail.com>
2017-03-30 16:20:08 +0100

    Finished.

--- Simple fun 22 Is Smooth.js
+++ Simple fun 22 Is Smooth.js
@@ -1,24 +1,9 @@
-function listSquared(m, n) {
-
-  var temp=0;
-  var answer=[];
-
-  for(j=m; j<n+1; j++){
-    temp=sumofsds(j);
-    if(Math.floor(Math.sqrt(temp))==Math.sqrt(temp)){answer.push([j,temp]);}
+function isSmooth(arr) {
+  if(arr[0]==arr[arr.length-1]){
+    if(arr.length%2==0){if(arr[0]==arr[arr.length/2]+arr[arr.length/2-1]){return true}}
+    if(arr.length%2==1){if(arr[0]==arr[(arr.length-1)/2]){return true}}
   }
+  return false;
 
-  return answer;
-
-  function sumofsds(a){
-    var count=0;
-    for(i=1; i<=Math.sqrt(a); i++){
-      if(a%i==0){
-        if(a/i != i){count=count+(i*i)+(a/i)*(a/i);}
-        else{count=count+i*i;}
-      }
-    }
-    return count;
-  }
 
 }
